60963370f17be669aa8c4adbe7cc9f24c8ebf8d5
kimwoolina <rladnfls0500@gmail.com>
2024-08-30 10:15:43 +0900

    feat: create api without using serializers

--- articles/views.py
+++ articles/views.py
@@ -11,12 +11,24 @@ from django.shortcuts import get_object_or_404
 
 # drf의 함수로 만드는 뷰는 반드시 api 데코레이터(Wrapping method - 함수 실행 전, 후에 실행해준다.) 필요
 #@api_view() 안에 안넣어주면 GET,  # ["GET","POST"]
-@api_view(["GET"])
+#게시글 목록, 생성 (서로 같은 URL)
+@api_view(["GET", "POST"])
 def article_list(request):
-    articles = Article.objects.all()
-    # articles가 1개보다 많을때는 반드시 many=True 적어줘야함
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    # 목록
+    if request.method == "GET":
+        articles = Article.objects.all()
+        # articles가 1개보다 많을때는 반드시 many=True 적어줘야함
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    # 생성
+    elif request.method == "POST":
+        data = request.data
+        title = data.get("title")
+        content = data.get("content")
+        article = Article.objects.create(title = title, content = content)
+        return Response({})
+        
 
 @api_view(["GET"])
 def article_detail(request, pk):
