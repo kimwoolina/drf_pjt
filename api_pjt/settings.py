054e2f8ad9befc6f1a4c3e93f52f8c5888f7f588
kimwoolina <rladnfls0500@gmail.com>
2024-09-04 23:20:53 +0900

    Debug mode off

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -20,7 +20,7 @@ OPEN_API_KEY = config.OPENAI_API_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 장고의 보안 기능.
 # 이 장고 프로젝트가 실행될 host를 적어줘야함.
