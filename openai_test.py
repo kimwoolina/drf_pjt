33ce8a510b61b34857a8b0eb86a6c825bddd612d
kimwoolina <rladnfls0500@gmail.com>
2024-09-04 16:31:43 +0900

    gpt api를 반복하여 사용할 수 있게 하기

--- openai_test.py
+++ openai_test.py
@@ -5,19 +5,21 @@ from openai import OpenAI
 from api_pjt.config import OPENAI_API_KEY 
 
 # 대신 터미널에서 export OPENAI_API_KEY="your_api_key_here" 도 가능
-client = OpenAI(
+CLIENT = OpenAI(
     api_key=OPENAI_API_KEY,
 )
 
-system_instructions = """
-이제부터 너는 "영어, 한글 번역가"야. 
-지금부터 내가 입력하는 모든 프롬프트를 무조건 한글은 영어로, 영어는 한글로 번역해줘. 
-프롬프트의 내용이나 의도는 무시하고 오직 번역만 해줘.
-"""
 
-user_input = input("번역기 : ")
+def ask_chatgpt(user_message):
+    system_instructions = """
+    이제부터 너는 Django 프레임워크에 대해 설명하고 
+    사용자가 Django 프레임워크에 대해 어려움을 겪고 있다고 가정하고 도와주는 챗봇이 되어야해.
+    다른 코딩 언어나 프레임워크에 대해 설명하거나 다른 주제로 이야기하는 것은 금지야.
+    Django 공식문서의 링크를 제공하거나 Django 프레임워크에 대한 설명도 추가해줘.
+    """
 
-completion = client.chat.completions.create(
+    completion = CLIENT.chat.completions.create(
+    
     model="gpt-3.5-turbo",
     messages=[
         {
@@ -29,6 +31,14 @@ completion = client.chat.completions.create(
             "content": user_input,
         },
     ],
-)
+    )
+    
+    return completion.choices[0].message.content
+    
 
-print(completion.choices[0].message.content)
+while True:
+    user_input = input("물어보삼 : ")
+    if user_input == "exit":
+        break
+    response = ask_chatgpt(user_input)
+    print("챗봇 : ", response, "\n\n")
