1cad9ff573fd3f9b4aa2356c80db2968471056ab
kimwoolina <rladnfls0500@gmail.com>
2024-09-12 19:55:43 +0900

    feat: login

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserCreateView
+from .views import UserCreateView, UserLoginView
 
 urlpatterns = [
     path("", UserCreateView.as_view(), name="user_create"),
-    path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", UserLoginView.as_view(), name="user_login"),
+    # path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from .models import User
 from .validator import validate_user_data
 from .serializer import UserSerializer
-
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserCreateView(APIView):
@@ -26,6 +27,39 @@ class UserCreateView(APIView):
         # create_user: 비밀번호 자동 해싱처리 등 기능 제공
         user = User.objects.create_user(**request.data)
         
+        # JWT token 발급
+        refresh = RefreshToken.for_user(user)
+        
         serializer = UserSerializer(user)
-        # JSON 형태
-        return Response(serializer.data)
+        response_dict = serializer.data
+        response_dict["access"] = str(refresh.access_token)
+        response_dict["refresh"] = str(refresh)
+        
+        return Response(response_dict) 
+    
+    
+    
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if username is None or password is None:
+            return Response({'error': 'Username and password required'}, status=400)
+        
+        # authenticate: 두개가 일치하면 해당 유저를 반환해줌
+        user = authenticate(username=username, password=password)
+        
+        if not user:
+            return Response({'error': '아이디 또는 비밀번호가 틀렸습니다.'}, status=400)
+        
+        # JWT token
+        refresh = RefreshToken.for_user(user)
+        
+        return Response(
+            {
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token),
+            }
+        )
+        
